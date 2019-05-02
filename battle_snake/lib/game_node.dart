cd2092561c79c20b484f1606941061125fe638d1
Brayan Montanez <bbmo.181299gmail.com>
2019-05-02 18:17:49 -0400

    Added auto movement and rotation logic

--- battle_snake/lib/game_node.dart
+++ battle_snake/lib/game_node.dart
@@ -10,9 +10,16 @@ class GameNode extends NodeWithSize {
   VirtualJoystick _joystick;
   Snake _snake;
   ImageMap _imageMap;
+  GradientNode _background;
 
+  GameNode(this._gameState, this._imageMap): super(new Size(400.0,400.0)) {
 
-  GameNode(this._gameState, this._imageMap): super(new Size(320.0, 320.0)) {
+    _background = new GradientNode(
+      this.size,
+      Color.fromRGBO(0, 0, 70, 1),
+      Color.fromRGBO(28, 181, 224, 1),
+    );
+    addChild(_background);
 
     _gameScreen = new Node();
     addChild(_gameScreen);
@@ -20,18 +27,14 @@ class GameNode extends NodeWithSize {
     _joystick = new VirtualJoystick();
     _joystick.scale = 0.40;
 
-
     _snake = new Snake(_imageMap);
 
-   // _joystick.position = new Offset(150.0, -350.0);
     _gameScreen.addChild(_joystick);
     _gameScreen.addChild(_snake);
-
   }
 
   void update(double n){
     _snake.moveSnake(_joystick.value);
-    //print(_joystick.value);
   }
 
   void spriteBoxPerformedLayout() {
@@ -42,12 +45,28 @@ class GameNode extends NodeWithSize {
 }
 
 
-class Level extends Node {
-  Snake snake;
 
-  Level() {
-    position = new Offset(160.0, 0.0);
+class GradientNode extends NodeWithSize {
+  GradientNode(Size size, this.colorTop, this.colorBottom) : super(size);
+
+  Color colorTop;
+  Color colorBottom;
+
+  @override
+  void paint(Canvas canvas) {
+    applyTransformForPivot(canvas);
+
+    Rect rect = Offset.zero & size;
+    Paint gradientPaint = new Paint()..shader = new LinearGradient(
+        begin: FractionalOffset.topLeft,
+        end: FractionalOffset.topRight,
+        colors: <Color>[colorTop, colorBottom],
+        stops: <double>[0.0, 1.0]
+    ).createShader(rect);
+
+    canvas.drawRect(rect, gradientPaint);
   }
 }
+
 
 
--- battle_snake/lib/snake.dart
+++ battle_snake/lib/snake.dart
@@ -3,6 +3,7 @@ part of game;
 class Snake extends Node{
  ImageMap _imageMap;
  Sprite _sprite;
+ double _dx, _dy;
 
  Snake(this._imageMap){
    _sprite = Sprite.fromImage(_imageMap['assets/Icon.png']);
@@ -11,22 +12,28 @@ class Snake extends Node{
 
    addChild(_sprite);
    position = new Offset(160.0, -160.0);
-   print('${_sprite}');
+   _dx = 0;
+   _dy = 1;
  }
 
  void moveSnake(Offset joystickValue){
    Offset oldPos = position;
-   Offset target = new Offset(oldPos.dx + (joystickValue.dx*5),oldPos.dy + (joystickValue.dy*5));
-   double filterFactor = 0.2;
+   Offset target;
+   if(joystickValue.dx == 0 && joystickValue.dy == 0) {
+     target = new Offset(oldPos.dx + (_dx * 3),oldPos.dy + (_dy * 3));
+   }
+   else {
+     target = new Offset(oldPos.dx + (joystickValue.dx * 5),oldPos.dy + (joystickValue.dy * 5) );
+     _sprite.rotation = atan2(joystickValue.dy,joystickValue.dx) * (180 / 3.14);
+     _dx = joystickValue.dx; _dy = joystickValue.dy;
+   }
 
-   _sprite.rotation = atan2(joystickValue.dy,joystickValue.dx)*(180/3.14);
+     double filterFactor = 0.2;
 
-   print(joystickValue.dy);
-
-   position = new Offset(
-       GameMath.filter(oldPos.dx, target.dx, filterFactor),
-       GameMath.filter(oldPos.dy, target.dy, filterFactor)
-   );
+     position = new Offset(
+         GameMath.filter(oldPos.dx, target.dx, filterFactor),
+         GameMath.filter(oldPos.dy, target.dy, filterFactor)
+     );
  }
 
 }
